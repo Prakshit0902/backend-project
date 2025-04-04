7b55c10bc9b13f13319a3e65721282fc0288c00c
Prakshit0902 <s.prakshit2004@gmail.com>
2025-04-04 22:20:39 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,54 @@
+// require('dotenv').config({path : './env'}) but this degrades consistency of code but there is no issue in code
+// to fix the above issue we can just add import statement and changing the package.json file 
+// by adding some commands in the scripts tag which are as 
+// dev : nodemon src/index.js  ---> dev : nodemon -r dotenv/config --experimental-json-modules src/index.js
+// 
+import dotenv from "dotenv"
+
+import {connectDB} from "./db/index.js"
+
+dotenv.config({
+    path : './.env'
+})
+
+connectDB()
+
+
+// method 1
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants"
+
+// import express from "express";
+
+// // function connectDB(){
+
+// // }
+
+// // connectDB()
+
+// /** 
+//  * But iife (immediately invoked function) which executes immediately as the program is live 
+//  * also a semicolon is used at starting of the iife for clean code purposes
+//  **/
+
+// const app = express()
+
+// ;(async () => {
+
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error) => {
+//             console.log("Error listening db " , error);
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`PORT : ${process.env.PORT}`);
+            
+//         }) 
+//     } catch (error) {
+//         console.log(error);
+        
+//     }
+
+// }) ()
